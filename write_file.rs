0a74b593db7af19927d34f3b9352d1b5d7fe703b
Brian Duffey <briand@mrc-productivity.com>
2018-02-23 17:17:32 -0600

    Added write file function

--- write_file.rs
+++ write_file.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{ Read, Error };
+use std::io::{ Read, Write, Error };
 
 fn read_file(path: &str) -> Result<String, Error> {
     let mut file = try!(File::open(path));
@@ -9,11 +9,19 @@ fn read_file(path: &str) -> Result<String, Error> {
     Ok(data)
 }
 
+fn write_file(path: &str, data: &str) -> Result<(), Error> {
+    let mut file = try!(File::create(path));
+
+    try!(file.write_all(data.as_bytes()));
+    Ok(())
+}
+
 fn main() {
     let hello = read_file("hello.txt").expect("Couldn't read 'hello.txt'.");
     let world = read_file("world.txt").expect("Couldn't read 'world.txt'.");
 
     let hello_world = format!("{} {}!", hello, world);
 
-    println!("Content is: {}", hello_world);
+    write_file("hello-world.txt", &hello_world).expect("Couldn't write 'hello-world.txt'.");
+    println!("Wrote file 'hello-world.txt' with content: {}", hello_world);
 }
