447894a51379076cf4f7d29bab0ef7749c21f19d
Brian Duffey <briand@mrc-productivity.com>
2018-02-23 17:11:56 -0600

    Error handling with custom message

--- write_file.rs
+++ write_file.rs
@@ -2,10 +2,10 @@ use std::fs::File;
 use std::io::Read;
 
 fn read_file(path: &str) -> String {
-    let mut file = File::open(path).unwrap();
+    let mut file = File::open(path).expect("Couldn't open file.");
     let mut data = String::new();
 
-    file.read_to_string(&mut data).unwrap();
+    file.read_to_string(&mut data).expect("Couldn't read file");
     data
 }
 
